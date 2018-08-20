6d2c9f6142851a97f81e3d21b33191c7ab431dea
Emilija <emilija.gjorgievska@outlook.com>
2018-08-20 15:57:04 +0200

    Create connection with database and create query

--- API/app.js
+++ API/app.js
@@ -1,10 +1,32 @@
 var express = require ('Express');
 var app = express();
+var pg = require('pg');
 
+const client = new pg.Client({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'thrift_shop',
+    password: 'admin',
+    port: 5432,
+  })
 
+  
+async function funkcija () {
+    
+     await client.connect()
+try {
+    var result = await client.query('SELECT * FROM T_USER');
+    console.log(result.rows[0]);
+} catch (err) {
+console.log('There is an error');
+} finally {
+await client.end();
+} }
+
+funkcija();
 
 
 
 app.listen(80, () => {
-    console.log("Listening on port 80");
+   console.log("Listening on port 80");
 } )
